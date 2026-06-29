f263d2bf6815bd1562892ccf2da43802b1d86b9a
aimad <moustajil.dev@gmail.com>
2026-06-29 11:05:46 +0100

    addine login and regester form and animation

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-"use strict"
+
+"use strict";
+
+const showLoginButton = document.querySelector("#showLogin");
+const showRegisterButton = document.querySelector("#showRegister");
+
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
+
+const registerLink = document.querySelector("#registerLink");
+const loginLink = document.querySelector("#loginLink");
+
+
+function showLoginForm() {
+  loginForm.classList.add("active-form");
+  registerForm.classList.remove("active-form");
+
+  showLoginButton.classList.add("active");
+  showRegisterButton.classList.remove("active");
+}
+
+
+function showRegisterForm() {
+  registerForm.classList.add("active-form");
+  loginForm.classList.remove("active-form");
+
+  showRegisterButton.classList.add("active");
+  showLoginButton.classList.remove("active");
+}
+
+
+showLoginButton.addEventListener("click", showLoginForm);
+
+showRegisterButton.addEventListener("click", showRegisterForm);
+
+registerLink.addEventListener("click", showRegisterForm);
+
+loginLink.addEventListener("click", showLoginForm);
